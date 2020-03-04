e826d275880e7785300f113fe6443434db24d307
ojripley <ojripley19@gmail.com>
2020-03-04 16:24:36 -0500

    add splash screen

---
+++ Assets/SplashAudioPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SplashAudioPlayer : MonoBehaviour {
+  // Start is called before the first frame update
+  void Start() {
+		DontDestroyOnLoad(this.gameObject);
+		Invoke("LoadGame", 3f);
+  }
+
+	private void LoadGame() {
+		SceneManager.LoadScene(1);
+	}
+}
